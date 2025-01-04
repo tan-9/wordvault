1c7bcfccf566d7fb00e2997d8a67296f6a29d7f1
Tanaya <tanayamohanty0909@gmail.com>
2025-01-05 00:18:39 +0530

    commit

--- components/DisplayFormedWords.jsx
+++ components/DisplayFormedWords.jsx
@@ -2,7 +2,7 @@ import React, {useState, useRef, useEffect, useCallback, useMemo} from "react";
 const DisplayFormedWords = ({foundWords}) => {
     return (
         <div className="flex flex-col justify-center items-center">
-            <div className="text-lg mb-2">Found Words:</div>
+            <div className="text-lg mb-3">Found Words:</div>
             <div className="font-bold">{foundWords.map((w, idx) => (
                 <div key={idx}>
                     {w}
