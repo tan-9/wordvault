5118d8b0d6335b805a8e40993801ddb1fdbf068a
Tanaya <tanayamohanty0909@gmail.com>
2025-01-04 17:17:51 +0530

    UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,20 @@ function App() {
             Drag to connect letters and form words!
           </p>
         </header>
-        <main className="flex flex-col justify-center container gap-4 mx-auto px-4"> 
-        <div className="h-12 text-3xl text-center tracking-widest items-center align-middle mx-auto relative overflow-hidden">
-          <Display displayLetters={selectedLetters} />
+        <div className="flex flex-col items-end container w-1/2">
+          <div className="max-w-xl bg-pink-200 max-h-lvh mx-auto rounded-xl shadow-2xl p-4 mb-2 relative overflow-hidden">
+            FORMED WORDS
+          </div>
         </div>
-        <div className="max-w-xl bg-white max-h-lvh mx-auto rounded-xl shadow-2xl p-4 mb-2 relative overflow-hidden">
-          <Grid selectedLetters={selectedLetters}
-          setSelectedLetters={setSelectedLetters} />
+        <div className="flex flex-col items-start container w-1/2"> 
+          <div className="h-12 text-3xl text-center tracking-widest items-center align-middle mx-auto relative overflow-hidden">
+            <Display displayLetters={selectedLetters} />
+          </div>
+          <div className="max-w-xl bg-white max-h-lvh mx-auto rounded-xl shadow-2xl p-4 mb-2 relative overflow-hidden">
+            <Grid selectedLetters={selectedLetters}
+            setSelectedLetters={setSelectedLetters} />
+          </div>
         </div>
-
-      </main>
       </div>
     </div>
   );
