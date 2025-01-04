e9e07381ce9c835659c32aa977cc09e6a81b06ed
Tanaya <tanayamohanty0909@gmail.com>
2025-01-04 15:39:07 +0530

    display fixed

--- components/Display.jsx
+++ components/Display.jsx
@@ -1,8 +1,8 @@
 import React, {useState, useRef, useEffect, useCallback, useMemo} from "react";
 const Display = ({displayLetters}) => {
     return (
-        <div className="relative">
-            <div className="text-pink-400">{displayLetters.map((letter) => letter.letter).join('')}</div>
+        <div className="flex justify-center items-center">
+            <div className="font-bold">{displayLetters.map((letter) => letter.letter).join('')}</div>
         </div>
     )
 }
