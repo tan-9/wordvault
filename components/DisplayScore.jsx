513bc4b51656ef3331522d7a1a2e5ff4c4686d90
Tanaya <tanayamohanty0909@gmail.com>
2025-01-10 19:02:27 +0530

    minor UI changes

--- components/DisplayScore.jsx
+++ components/DisplayScore.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 
 const DisplayScore = ({totalScore, validWords, setValidWords}) => {
     return(
-        <div className="bg-white rounded-xl shadow-2xl p-4 flex flex-col items-center">
+        <div className="bg-white rounded-xl shadow-2xl p-6 flex flex-col items-center" style={{fontFamily: 'poppins'}}>
             <div>You Scored {totalScore} points</div>
             <div className="mx-2"> <hr className="pb-2 border-t border-gray-300"/></div>
-            <div className="mt-2"> 
+            <div className="mt-3 flex flex-col gap-2"> 
                 {validWords.map(({ word, score }, idx) => (
                     <div key={idx} className="font-bold flex flex-row">
                         {word}
--- src/App.jsx
+++ src/App.jsx
@@ -64,101 +64,103 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="min-h-screen flex flex-col items-center bg-stone-300">
-      <div className="flex flex-col items-center px-4 py-8">
-        
-        {!isGameplayed && (
-          <div>
-            <header className="text-center">
-              <h1 className="font-outfit font-medium text-5xl mb-4">Word Grid Game</h1>
-              <p className="font-poppins text-lg text-gray-800">
-                Drag to connect letters and form words!
-              </p>
-            </header>
-          <div className="bg-white mt-11 p-6 flex flex-col items-center gap-2 rounded-xl shadow-2xl">
-            <GameRoom 
-              socket={socket}
-              isTimerActive={isTimerActive}
-              setisTimerActive={setisTimerActive}
-              hasJoinedRoom={hasJoinedRoom} 
-              setHasJoinedRoom={setHasJoinedRoom} 
-              setRoomId={setRoomId}
-            />
-          </div>
-          </div>
-        )}
+        <div className="flex flex-col items-center px-4 py-8">
+          
+          {!isGameplayed && (
+            <div>
+              <header className="text-center">
+                <h1 className="font-outfit font-medium text-5xl mb-4">Word Grid Game</h1>
+                <p className="font-poppins text-lg text-gray-800">
+                  Drag to connect letters and form words!
+                </p>
+              </header>
+            <div className="bg-white mt-11 p-6 flex flex-col items-center gap-2 rounded-xl shadow-2xl">
+              <GameRoom 
+                socket={socket}
+                isTimerActive={isTimerActive}
+                setisTimerActive={setisTimerActive}
+                hasJoinedRoom={hasJoinedRoom} 
+                setHasJoinedRoom={setHasJoinedRoom} 
+                setRoomId={setRoomId}
+              />
+            </div>
+            </div>
+          )}
 
-        {isTimerActive &&  (
-            <div className="flex flex-col justify-center items-center self-center">
-              <div className="flex flex-col md:flex-row items-center justify-center space-x-4 w-full max-w-5xl bg-white shadow-xl rounded-lg p-6">
-                <div className="flex justify-between container space-x-20 w-full"> 
-                  <div className="flex flex-col items-start w-1/2"> 
-                    <div className="h-12 text-2xl text-center tracking-widest self-center justify-center ml-20 text-center align-middle overflow-hidden">
-                      <Display displayLetters={selectedLetters} />
-                    </div>
-                    <div className="w-max p-4 rounded-lg flex bg-slate-200 flex-col items-center">
-                      <div className="w-full">
-                        <Grid
-                          selectedLetters={selectedLetters}
-                          setSelectedLetters={setSelectedLetters}
-                          foundWords={foundWords}
-                          setFoundWords={setFoundWords}
-                        />
+          {isTimerActive &&  (
+              <div className="flex flex-col justify-center items-center self-center">
+                <div className="bg-white rounded-md my-3 p-3 w-48 flex flex-col items-center justify-center">
+                    <Timer 
+                        socket={socket}
+                        isTimerActive={isTimerActive} 
+                        setisTimerActive={setisTimerActive}
+                        roomId={roomId}
+                      />
+                  </div>
+                <div className="flex md:flex-row items-center justify-center space-x-4 w-full max-w-5xl bg-white shadow-xl rounded-lg p-6">
+                  <div className="flex justify-between container space-x-20 w-full"> 
+                    <div className="flex flex-col items-start w-1/2"> 
+                      <div className="h-12 text-2xl text-center tracking-widest self-center justify-center ml-20 text-center align-middle overflow-hidden">
+                        <Display displayLetters={selectedLetters} />
+                      </div>
+                      <div className="w-max p-4 rounded-lg flex bg-slate-200 flex-col items-center">
+                        <div className="w-full">
+                          <Grid
+                            selectedLetters={selectedLetters}
+                            setSelectedLetters={setSelectedLetters}
+                            foundWords={foundWords}
+                            setFoundWords={setFoundWords}
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="flex flex-col items-start w-1/3 bg-green-100 p-4 rounded-lg">
-                    <h2 className="text-lg font-semibold mb-4 font-outfit">Words Formed:</h2>
-                    <DisplayFormedWords
-                      foundWords={foundWords}
-                      totalScore={totalScore}
-                      setTotalScore={setTotalScore}
-                      validWords={validWords}
-                      setValidWords={setValidWords}
-                    />
+                    <div className="flex flex-col items-start w-1/3 bg-green-100 p-4 rounded-lg">
+                      <h2 className="text-lg font-semibold mb-4 font-outfit">Words Formed:</h2>
+                      <DisplayFormedWords
+                        foundWords={foundWords}
+                        totalScore={totalScore}
+                        setTotalScore={setTotalScore}
+                        validWords={validWords}
+                        setValidWords={setValidWords}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="bg-white rounded-md my-3 p-3 w-48 flex flex-col items-center justify-center">
-                <Timer 
-                    socket={socket}
-                    isTimerActive={isTimerActive} 
-                    setisTimerActive={setisTimerActive}
-                    roomId={roomId}
-                  />
-              </div>
-            </div>
-        )}
+          )}
 
-        {isGameplayed && !isTimerActive && (
-          <div className="ml-1 mt-3 flex flex-col gap-4 text-center">
-            <DisplayScore 
-              totalScore={totalScore} 
-              validWords={validWords} 
-              setValidWords={setValidWords}
-            />
-            <button
-              onClick={() => {
-                resetGame();
-                setisGameplayed(false);
-              }}
-              style={{
-                display: 'inline-block',
-                paddingLeft: '25px',
-                paddingRight: '25px',
-                paddingTop: '15px',
-                paddingBottom: '15px',
-                alignItems: 'center',
-                justifyContent: 'center',
-                marginTop: '6px',
-                boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.3)",
-                borderRadius: '100px',
-                backgroundColor:  "#c2fbd7"
-            }}>
-              Play Again
-            </button>
-          </div>
-        )}
-      </div>
+          {isGameplayed && !isTimerActive && (
+            <div className="ml-1 mt-8 flex flex-col gap-4 text-center">
+              <DisplayScore 
+                totalScore={totalScore} 
+                validWords={validWords} 
+                setValidWords={setValidWords}
+              />
+              <button
+                onClick={() => {
+                  resetGame();
+                  setisGameplayed(false);
+                }}
+                style={{
+                  display: 'inline-block',
+                  fontFamily: 'poppins',
+                  fontSize: '20px',
+                  paddingLeft: '20px',
+                  paddingRight: '20px',
+                  paddingTop: '14px',
+                  paddingBottom: '13px',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  marginTop: '6px',
+                  boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.3)",
+                  borderRadius: '100px',
+                  backgroundColor:  "#c2fbd7"
+              }}>
+                PLAY AGAIN
+              </button>
+            </div>
+          )}
+        </div>
     </div>
     </BrowserRouter>
   );
@@ -167,120 +169,3 @@ const App = () => {
 export default App;
 
 
-// function App() {
-//   const [selectedLetters, setSelectedLetters] = useState([]);
-//   const [foundWords, setFoundWords] = useState([]);
-//   const [isTimerActive, setisTimerActive] = useState(false);
-//   const [isGameplayed, setisGameplayed] = useState(false);
-//   const [totalScore, setTotalScore] = useState(0);
-//   const [validWords, setValidWords] = useState([]);
-//   const [hasJoinedRoom, setHasJoinedRoom] = useState(false);
-//   const [roomId, setRoomId] = useState("");
-
-//   const resetGame = () => {
-//     setSelectedLetters([]);
-//     setFoundWords([]);
-//     setTotalScore(0);
-//     setValidWords([]);
-//   };
-
-//   const startNewGame = () => {
-//     resetGame();
-//     setisTimerActive(true);
-//     setisGameplayed(true);
-//   };
-
-//   const stopGame = () => {
-//     setisTimerActive(false);
-//     setisGameplayed(true);
-//   };
-
-//   return (
-//     <div className="min-h-screen flex flex-col items-center bg-stone-300">
-//       <div className="flex flex-col items-center px-4 py-8">
-//         <header className="text-center mb-8">
-//           <h1 className="text-3xl font-bold mb-4">Word Grid Game</h1>
-//           <p className="text-gray-600">
-//             Drag to connect letters and form words!
-//           </p>
-//         </header>
-        
-//         {
-//           !isGameplayed && (
-//             <GameRoom isTimerActive={isTimerActive} setisTimerActive={(isTimerActive) => isTimerActive ? startNewGame() : stopGame() } 
-//                       hasJoinedRoom={hasJoinedRoom} setHasJoinedRoom={setHasJoinedRoom} setRoomId={setRoomId} />
-//           )
-//         }
-
-//         {isTimerActive &&  (
-//           <div className="flex flex-col md:flex-row items-center justify-center space-x-4 w-full max-w-5xl bg-white shadow-xl rounded-lg p-6">
-//             <div className="flex justify-between container space-x-20 w-full"> 
-//             <Timer isTimerActive={isTimerActive} setisTimerActive={(isTimerActive) => isTimerActive ? startNewGame() : stopGame()} roomId={roomId} />
-//               <div className="flex flex-col items-start w-1/2"> 
-//                 <div className="h-12 text-2xl text-center tracking-wider items-center align-middle mx-auto relative overflow-hidden">
-//                   <Display displayLetters={selectedLetters} />
-//                 </div>
-//                 <div className="w-max p-4 rounded-lg flex bg-slate-200 flex-col items-center">
-//                   <div className="w-full">
-//                     <Grid
-//                       selectedLetters={selectedLetters}
-//                       setSelectedLetters={setSelectedLetters}
-//                       foundWords={foundWords}
-//                       setFoundWords={setFoundWords}
-//                     />
-//                   </div>
-//                 </div>
-//               </div>
-//               <div className="flex flex-col items-start w-1/3 bg-green-100 p-4 rounded-lg">
-//                 <h2 className="text-lg font-semibold mb-4">Words Formed:</h2>
-//                 <DisplayFormedWords
-//                   foundWords={foundWords}
-//                   totalScore={totalScore}
-//                   setTotalScore={setTotalScore}
-//                   validWords={validWords}
-//                   setValidWords={setValidWords}
-//                 />
-//               </div>
-//             </div>
-//           </div>
-//         )}
-
-//         {isGameplayed && !isTimerActive && (
-//           <div className="ml-1 mt-3 flex flex-col gap-4 text-center"> 
-//             <DisplayScore totalScore={totalScore} validWords={validWords} setValidWords={setValidWords}/> 
-//             <div>
-//               <div className="p-2 rounded-lg">
-//                 <button onClick={() => {
-//                   resetGame();
-//                   setisGameplayed(false);
-//                 }}
-//                 style={
-//                   {
-//                     backgroundColor: "#c2fbd7",
-//                     paddingLeft: '11px',
-//                     paddingRight: '11px',
-//                     paddingTop: '8px',
-//                     paddingBottom: '8px',
-//                     borderRadius: '20px',
-//                     boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.3)"
-//                   }
-//                 }>
-//                   <div className="pl-4">
-//                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
-//                       <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
-//                     </svg>
-//                   </div>
-//                   <div className="text-sm">go home</div>
-//                 </button>
-//               </div>              
-//             </div>
-//           </div>
-
-//         )}
-//       </div>
-//     </div>
-
-//   );
-// }
-
-// export default App;
