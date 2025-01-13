26e946d55c5be022356da57f92cbc58abdb3fa48
Tanaya <tanayamohanty0909@gmail.com>
2025-01-13 23:18:58 +0530

    UI changes

--- components/DisplayScore.jsx
+++ components/DisplayScore.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 const DisplayScore = ({totalScore, validWords, setValidWords}) => {
     return(
         <div className="bg-white rounded-xl shadow-2xl p-6 flex flex-col items-center" style={{fontFamily: 'poppins'}}>
-            <div>You Scored {totalScore} points</div>
+            <div>You Scored <b>{totalScore}</b> points</div>
             <div className="mx-2"> <hr className="pb-2 border-t border-gray-300"/></div>
             <div className="mt-3 flex flex-col gap-2"> 
                 {validWords.map(({ word, score }, idx) => (
